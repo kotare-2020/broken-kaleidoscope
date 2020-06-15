bc3f54ba5b5a3ab307deef15ab22dea23cf8331d
student <student@eda-bc-05.localdomain>
2020-06-16 10:02:13 +1200

    Get div stylish

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 class Pixel extends React.Component {
-  render() { return <div></div> }
+  render() {
+    return (
+      <div style={{height: '50px', width: '50px', backgroundColor: 'green' }}>
+
+      </div>
+    )
+  }
 }
 
 export default Pixel
