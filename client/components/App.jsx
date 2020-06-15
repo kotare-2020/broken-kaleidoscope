4619f282ca1c9db9c2df5b7110bd5e77c4e8e7b8
Richard E <46254658+richard-eman@users.noreply.github.com>
2020-06-16 10:13:05 +1200

    make pixel and show it

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Pixel from './Pixel.jsx'
 
 const App = () => {
   return (
-    <div>React development has begun!</div>
+    <>
+      <Pixel />
+    </>
   )
 }
 
---
+++ client/components/Pixel.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Pixel = props => {
+  return <div style={
+    // dodgerblue, cornflowerblue, lightsteelblue https://www.rapidtables.com/web/css/css-color.html#blue good shit
+    { backgroundColor: 'cornflowerblue', height: '30px', width: '30px' }
+  }></div>
+}
+
+export default Pixel
