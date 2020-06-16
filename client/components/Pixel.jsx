11cba8622e24adf8ec9549c15e1e466078cf2307
student <student@eda-bc-05.localdomain>
2020-06-16 15:56:23 +1200

    Get seizure-inducing rainbow animation working

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -6,23 +6,53 @@ const randomHexColor = () =>
 class Pixel extends React.Component {
   state = {
     style: {
-      height: '10px',
-      width: '10px',
-      backgroundColor: randomHexColor()
-    }
+      height: '40px',
+      width: '40px',
+      backgroundColor: randomHexColor(),
+      animation: '',
+       animationIterationCount: 'infinite'
+    }    
   }
 
-  clickHandler = () => {
+  enterEvent = () => {
     this.setState({
       style: {
-        ...this.state.style, backgroundColor: randomHexColor()
+        ...this.state.style, animation: 'rainbow-bg infinite 5s linear',
+         
+      }
+    })
+  }
+  
+   contextEvent = (evt) => {
+    evt.preventDefault()
+    this.setState({
+      style: {
+        ...this.state.style, backgroundColor: "#000000"
       }
     })
   }
 
+  doubleEvent = () => {
+    this.setState({
+      style: {
+        ...this.state.style, backgroundColor: "#FFFFFF"
+      }
+    })
+  }
+
+  dragEvent = (e) => {
+    console.log(e)
+    this.setState({
+      style: {
+        ...this.state.style, backgroundColor: "#FFFF00"
+      }
+    })
+  }
+
+
   render() {
     return (
-      <div style={this.state.style} onMouseEnter={this.clickHandler}>
+      <div style={this.state.style} onMouseEnter={this.enterEvent} onContextMenu={this.contextEvent} onDoubleClick={this.doubleEvent} onDragEnter={this.dragEvent} >
 
       </div>
     )
