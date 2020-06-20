872dec9423429df867e272037aa746354c36a37a
George-Staniland <georgestaniland@gmail.com>
2020-06-20 19:39:17 +1200

    init commit

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Pixel extends React.Component {
+    render () {
+        return (
+            <div style={{height: "100px" , width: "100px", backgroundColor: "Crimson" }}> </div>
+        )
+        
+    }
+}
+
+export default Pixel
